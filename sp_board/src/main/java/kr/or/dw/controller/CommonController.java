d160fef19dd9028081110c9b6eb85848be35c717
pjkm00 <pjkm00@gmail.com>
2023-06-13 16:56:59 +0900

    .

--- sp_board/src/main/java/kr/or/dw/controller/CommonController.java
+++ sp_board/src/main/java/kr/or/dw/controller/CommonController.java
@@ -1,15 +1,25 @@
 package kr.or.dw.controller;
 
-import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.or.dw.service.MemberService;
 
 @Controller
 public class CommonController {
 
+	@Autowired
+	private MemberService memberService;
+	
 //	@RequestMapping("/common/loginForm")
 	@GetMapping("/common/loginForm")
 //	@PostMapping("/common/loginForm")
@@ -18,5 +28,27 @@ public class CommonController {
 		
 		return url;
 		
+	}
+	
+	@RequestMapping("/common/login")
+	public String login(String id, String pwd, HttpSession session) throws SQLException{
+		String url = "redirect:/index.do";
+		
+		memberService.login(id, pwd, session);
+		
+		
+		
+		return url;
+	}
+	
+	@RequestMapping("/index")
+	public ModelAndView index(ModelAndView mnv) {
+		String url = "";
+	
+		
+//		mnv.addObject("menuList", menuList);
+		mnv.setViewName(url);
+		
+		return mnv;
 	}
 }
